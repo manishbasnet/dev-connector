bd76be98fe6e2707fc5f93d0682ae237f9f1056e
Manish Basnet <manish.basnetnp@gmail.com>
2019-01-06 20:20:43 +0545

    validation rule added in register

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
  
 // Load User model
 const User = require('../../models/User');
@@ -19,6 +22,13 @@ router.get('/test', (req, res) => res.json({msg:"Users works"}));
 // @desc Register user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = 'Email already exists';
