532dce21b3fcc76f65943d8c9e53ca6b87d12e9a
Manish Basnet <manish.basnetnp@gmail.com>
2019-01-06 21:23:45 +0545

    login validation done

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 // Load Input Validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
  
 // Load User model
 const User = require('../../models/User');
@@ -27,7 +28,7 @@ router.post('/register', (req, res) => {
   // Check Validation
   if(!isValid) {
     return res.status(400).json(errors);
-  }
+  } 
 
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
@@ -66,6 +67,13 @@ router.post('/register', (req, res) => {
 // @access Public
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if(!isValid) {
+    return res.status(400).json(errors);
+  } 
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -74,7 +82,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if(!user) {
-        return res.status(404).json({email: 'User not found'});
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
 
       // Check Password
@@ -93,7 +102,8 @@ router.post('/login', (req, res) => {
                 })
             });
           } else {
-            return res.status(400).json({password: 'Password incorrect'});
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
           }
         })
     });
--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-const port = process.env.PORT || 5008;
+const port = process.env.PORT || 5009;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
